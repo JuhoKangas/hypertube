82f94fcdcf10472653b3fac0b954f55fdfc99211
Juho Kangas <jkangas@student.hive.fi>
2023-03-01 14:56:50 +0200

    Create middleware for unknown endpoint and request logger

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 
 // Routes
 const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
