ec2252ff6f5fa8436a69756a9446fc1e282c3f2a
Katarina Slotova <kslotova@c2r2p10.hive.fi>
2023-03-02 11:20:29 +0200

    Add login and signup routes.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Landing from './routes/Landing'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
 
 const App = () => {
   return (
@@ -9,9 +11,10 @@ const App = () => {
       <Router>
         <Routes>
           <Route path={'/'} element={<Landing />}></Route>
+          <Route path={'/login'} element={<Login />}></Route>
+          <Route path={'/signup'} element={<Signup />}></Route>
         </Routes>
       </Router>
-      <h1 className='text-xl'>Let's fucking go</h1>
     </div>
   )
 }
--- client/src/routes/Landing.js
+++ client/src/routes/Landing.js
@@ -1,8 +1,17 @@
 import React from 'react'
-//import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Landing = () => {
-  //const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  const navigateLogin = () => {
+    navigate('/login')
+  }
+
+  const navigateSignup = () => {
+    navigate('/signup')
+  }
+
   return (
     <div className='flex flex-col h-screen bg-cover bg-landing-bg'>
       <p className='text-white font-montserrat text-center font-thin mt-16 p-5 text-5xl'>
@@ -10,13 +19,19 @@ const Landing = () => {
       </p>
       <div className='flex flex-row justify-center gap-36 mt-40'>
         <div>
-          <button className='w-32  h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat'>
+          <button
+            className='w-32  h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat'
+            onClick={navigateLogin}
+          >
             Log in
           </button>
         </div>
         <div className='bg-white after:block after:w-[1px] after:h-10 after:mx-auto after:my-2'></div>
         <div>
-          <button className='w-32 h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat'>
+          <button
+            className='w-32 h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat'
+            onClick={navigateSignup}
+          >
             Sign up
           </button>
         </div>
