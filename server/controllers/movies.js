74754a1dd436b8b3f48d724facf8afff5e33f358
Juho Kangas <jkangas@student.hive.fi>
2023-03-02 16:11:21 +0200

    remove console logs from backend

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -5,10 +5,6 @@ moviesRouter.get('/', async (req, res) => {
   const allMovies = await axios.get(
     'https://yts.torrentbay.to/api/v2/list_movies.jsonp?sort_by=like_count'
   )
-
-  console.log('are we here')
-  console.log(allMovies)
-
   const stringified = JSON.stringify(allMovies.data.data)
 
   res.status(200).json(JSON.parse(stringified))
