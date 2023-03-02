98dd9e173d4e0a755dddab8129ae6de131cb0ae2
Katarina Slotova <katarina.slotova@gmail.com>
2023-03-02 13:49:35 +0200

    Responsive landing page.

--- client/src/routes/Landing.js
+++ client/src/routes/Landing.js
@@ -17,19 +17,19 @@ const Landing = () => {
       <p className='text-white font-montserrat text-center font-thin mt-16 p-5 text-5xl'>
         WATCH THE LATEST MOVIES ON HYPERTUBE
       </p>
-      <div className='flex flex-row justify-center gap-36 mt-40'>
+      <div className='flex flex-row justify-center md:gap-36 gap-3 mt-40'>
         <div>
           <button
-            className='w-32  h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat'
+            className='w-32 h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat border border-hyper-black md:border-none'
             onClick={navigateLogin}
           >
             Log in
           </button>
         </div>
-        <div className='bg-white after:block after:w-[1px] after:h-10 after:mx-auto after:my-2'></div>
+        <div className='md:bg-white after:block after:w-[1px] after:h-10 after:mx-auto after:my-2'></div>
         <div>
           <button
-            className='w-32 h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat'
+            className='w-32 h-12 bg-dark-red font-bold text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat border border-hyper-black md:border-none'
             onClick={navigateSignup}
           >
             Sign up
