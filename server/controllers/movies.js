572a12391538d3158ac5ef1fa37afcfed8bbe28c
Alba Castano <acastano@student.hive.fi>
2023-03-10 21:01:29 +0200

    Downloads controller, saving video to downloads/NAME_OF_VDEO/

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,10 +1,13 @@
 const moviesRouter = require('express').Router()
 const axios = require('axios')
+const path = require('path')
+const fs = require('fs')
+const { Console } = require('console')
 
 moviesRouter.get('/', async (req, res) => {
   console.log(req.query)
   const allMovies = await axios.get(
-    'https://yts.torrentbay.to/api/v2/list_movies.json?sort_by=like_count'
+    'https://yts.mx/api/v2/list_movies.json?sort_by=like_count'
   )
   const stringified = JSON.stringify(allMovies.data.data)
 
@@ -14,7 +17,7 @@ moviesRouter.get('/', async (req, res) => {
 moviesRouter.get('/page/:page', async (req, res) => {
   const page = req.params.page
   const movieData = await axios.get(
-    `https://yts.torrentbay.to/api/v2/list_movies.jsonp?sort_by=like_count&page=${page}`
+    `https://yts.mx/api/v2/list_movies.jsonp?sort_by=like_count&page=${page}`
   )
   const stringified = JSON.stringify(movieData.data.data)
 
@@ -24,7 +27,7 @@ moviesRouter.get('/page/:page', async (req, res) => {
 moviesRouter.get('/id/:id', async (req, res) => {
   const movieId = req.params.id
   const movieData = await axios.get(
-    `https://yts.torrentbay.to/api/v2/movie_details.json?movie_id=${movieId}&with_cast=true`
+    `https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}&with_cast=true`
   )
 
   const stringified = JSON.stringify(movieData.data.data)
@@ -34,15 +37,28 @@ moviesRouter.get('/id/:id', async (req, res) => {
 
 moviesRouter.get('/download/:id', async (req, res) => {
   const movieId = req.params.id
+  console.log(`params: `, req.params)
+  let downloadingFile = null
+  let resSent = false
+
+  const checkFileSize = (filePath) => {
+    const checkFileSize = () => {
+      const stats = fs.statSync(`${filePath}`)
+      return stats.size
+    }
+
+    if (fs.existsSync(`${filePath}`)) {
+      return checkFileSize()
+    }
+    return 0
+  }
+
   try {
     const ytsData = await axios.get(
-      `https://yts.torrentbay.to/api/v2/movie_details.json?movie_id=${movieId}&with_cast=true`
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}&with_cast=true`
     )
     const movieData = ytsData.data.data.movie
-    // const stringified = JSON.stringify(ytsData.data.data)
-    //filter movieData.torrents with 1080p
-    //check that something returned
-    //if more than 1 torrents, sort by seeds
+
     let torrents
     if (movieData.torrents.length > 0) {
       torrents = movieData.torrents.filter((t) => t.quality === '1080p')
@@ -51,45 +67,92 @@ moviesRouter.get('/download/:id', async (req, res) => {
           b.seeds - a.seeds
         })
       } else if (torrents.length < 1) {
-        //error no movies for that quality
-        res.status(200).json({ error: 'No torrents for 1080p quality' })
+        if (!resSent) {
+          resSent = true
+          res.status(200).json({ error: 'No torrents for 1080p quality' })
+        }
       }
-    } else {
-      //error no torrents for that movie
+    } else if (!resSent) {
+      resSent = true
       res.status(200).json({ error: 'No torrents found' })
     }
-    //do magnet link with hash from torrent
-    const firstTorrent = torrents[0]
+
     const longNamePlus = movieData.title_long.split(' ').join('+')
-    const magnetLink = `magnet:?xt=urn:btih:${firstTorrent.hash}&dn=${longNamePlus}`
-
-    console.log('magl: ', magnetLink)
-
-    // const trackers = [
-    //   'udp://opentracker.i2p.rocks:6969/announce',
-    //   'http://opentracker.i2p.rocks:6969/announce',
-    //   'udp://tracker.opentrackr.org:1337/announce',
-    //   'http://tracker.opentrackr.org:1337/announce',
-    //   'udp://tracker.internetwarriors.net:1337/announce',
-    //   'http://tracker.internetwarriors.net:1337/announce',
-    //   'udp://exodus.desync.com:6969/announce',
-    //   'udp://tracker.cyberia.is:6969/announce',
-    //   'udp://3rt.tace.ru:60889/announce',
-    //   'http://5rt.tace.ru:60889/announce',
-    //   'udp://explodie.org:6969/announce',
-    //   'http://explodie.org:6969/announce',
-    //   'udp://47.ip-51-68-199.eu:6969/announce',
-    //   'udp://tracker.tiny-vps.com:6969/announce',
-    // ]
+    const magnetLink = `magnet:?xt=urn:btih:${torrents[0].hash}&dn=${longNamePlus}`
 
     //require torrent-stream https://www.npmjs.com/package/torrent-stream
-    //make engine listeres for 'ready', 'download', 'idle'
-    //filter file types
-    //in ready file.select() to download
+    var torrentStream = require('torrent-stream')
+
+    var engine = torrentStream(magnetLink, {
+      trackers: [
+        'udp://opentracker.i2p.rocks:6969/announce',
+        'http://opentracker.i2p.rocks:6969/announce',
+        'udp://tracker.opentrackr.org:1337/announce',
+        'http://tracker.opentrackr.org:1337/announce',
+        'udp://tracker.internetwarriors.net:1337/announce',
+        'http://tracker.internetwarriors.net:1337/announce',
+        'udp://exodus.desync.com:6969/announce',
+        'udp://tracker.cyberia.is:6969/announce',
+        'udp://3rt.tace.ru:60889/announce',
+        'http://5rt.tace.ru:60889/announce',
+        'udp://explodie.org:6969/announce',
+        'http://explodie.org:6969/announce',
+        'udp://47.ip-51-68-199.eu:6969/announce',
+        'udp://tracker.tiny-vps.com:6969/announce',
+      ],
+      path: path.resolve(__dirname, '../downloads'),
+    })
+
+    engine.on('ready', function () {
+      const files = engine.files.filter(
+        (file) =>
+          file.name.substring(file.name.length - 3) === 'mkv' ||
+          file.name.substring(file.name.length - 3) === 'mp4' ||
+          file.name.substring(file.name.length - 4) === 'webm'
+      )
+      if (files.length > 0) {
+        downloadingFile = files[0]
+        console.log(`STARTING download of: `, downloadingFile.path)
+        console.log('That files full size is: ', downloadingFile.length)
+        downloadingFile.select()
+      } else {
+        engine.destroy()
+        if (!resSent) {
+          resSent = true
+          res.status(200).json({ error: 'No files with that extension' })
+        }
+      }
+    })
+
+    engine.on('download', function () {
+      const sizeDownloaded = checkFileSize(
+        `${path.resolve(__dirname, '../downloads')}/${downloadingFile.path}`
+      )
+      const soFar = (sizeDownloaded / downloadingFile.length) * 100
+      console.log('Downloaded so far: ', sizeDownloaded, ', ', soFar, '%')
+      if (soFar > 1.5 && soFar < 100 && !resSent) {
+        resSent = true
+        res.status(200).json({ msg: 'Over 1,5% downloaded, stream' })
+      }
+    })
+
+    engine.on('idle', function () {
+      console.log('Download compeleted, 100%, engine destroyed.')
+      if (downloadingFile === null && !resSent) {
+        resSent = true
+        res.status(200).json({ error: 'Download failed' })
+      } else if (!resSent) {
+        resSent = true
+        res.status(200).json({ msg: 'Download completed 100%' })
+      }
+      engine.destroy()
+    })
   } catch (e) {
     console.log('error in /download/:id: ', e)
-    // res.status(200).json(JSON.parse(movieData))
-    res.status(200)
+    if (!resSent) {
+      resSent = true
+      res.status(200)
+    }
   }
 })
 
